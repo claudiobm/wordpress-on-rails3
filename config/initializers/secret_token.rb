929bff9c9c93046d3c49eb368262a95177bcac1c
Claudio Bruno Martins <claudio.bruno.martins@gmail.com>
2010-11-01 11:23:18 -0200

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+WordpressOnRails::Application.config.secret_token = 'd42fa10b8022a7da06de0e6009e3928690d7885981608f935b35675e534064bdf02c2d5717c634529898425050ecbc7a8271008d065bd8db8a175fc2b5bf08e7'
